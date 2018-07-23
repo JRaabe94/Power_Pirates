8e7f7b2e30ecf6ab11602803e5d6c87b00893d71
Codecamp <Codecamp@mac-mini-8.local>
2018-07-23 02:30:02 -0700

    Initial Commit

---
+++ Power_Pirates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Power_Pirates
+//
+//  Created by Codecamp on 23.07.18.
+//  Copyright © 2018 Codecamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Power_PiratesTests/Power_PiratesTests.m
@@ -0,0 +1,39 @@
+//
+//  Power_PiratesTests.m
+//  Power_PiratesTests
+//
+//  Created by Codecamp on 23.07.18.
+//  Copyright © 2018 Codecamp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Power_PiratesTests : XCTestCase
+
+@end
+
+@implementation Power_PiratesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
