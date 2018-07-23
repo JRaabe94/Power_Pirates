d9a7464a19ad154dff3857e61c80ffdb5b3f6a93
Codecamp <Codecamp@mac-mini-8.local>
2018-07-23 02:49:02 -0700

    Chris stinkt

---
+++ Power_Pirates/pirate.h
@@ -0,0 +1,13 @@
+//
+//  pirate.h
+//  Power_Pirates
+//
+//  Created by Codecamp on 23.07.18.
+//  Copyright © 2018 Codecamp. All rights reserved.
+//
+
+#ifndef pirate_h
+#define pirate_h
+
+
+#endif /* pirate_h */
