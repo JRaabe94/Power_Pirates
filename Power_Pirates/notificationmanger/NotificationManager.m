ad38b1eb9b2eea342a4c02ef60f27416c570c57e
Codecamp <Codecamp@mac-mini-9.local>
2018-07-24 14:12:28 +0200

    Added method for push notifications

--- Power_Pirates/notificationmanger/NotificationManager.m
+++ Power_Pirates/notificationmanger/NotificationManager.m
@@ -2,8 +2,36 @@
 //  NotificationManager.m
 //  Power_Pirates
 //
-//  Created by Codecamp on 23.07.18.
+//  Created by Codecamp on 24.07.18.
 //  Copyright © 2018 Codecamp. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "NotificationManager.h"
+#import "ViewController.h"
+
+@implementation NotificationManager
+
+// Aufruf: [NotificationManager createPushNotification:@"Hallo!"];
+
++ (void)createPushNotification:(NSString *)message;
+// Creates a Push-notification with the given message
+{
+    bool isGrantedNotificationAccess = true;
+    if (isGrantedNotificationAccess) {
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+        content.title = @"Power Pirates";
+        // content.subtitle = @"Untertitel";
+        content.body = message;
+        content.sound = [UNNotificationSound defaultSound];
+        
+        UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:5 repeats:NO];
+        
+        // Setting up the request for notification
+        UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:@"UYLocalNotification" content:content trigger:trigger];
+        
+        [center addNotificationRequest:request withCompletionHandler:nil];
+    }
+}
+
+@end
