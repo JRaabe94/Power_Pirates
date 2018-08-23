d1f08742c58a11f837dcced19105460207c5bfb7
Codecamp <Codecamp@mac-mini-1.local>
2018-08-23 22:08:17 +0200

    Lebensanzeige klappt

--- Power_Pirates/viewcontroller/MainGameViewController.m
+++ Power_Pirates/viewcontroller/MainGameViewController.m
@@ -24,6 +24,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *desireText;
 @property bool menuShowing;
 
+@property AppDelegate *appDelegate;
 @property Storage *storage;
 @property Pirates *pirat;
 
@@ -37,7 +38,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self viewLoadSetup];
+    [self viewInitiateSetup];
 }
 
 // this Method will be called everytime the main View is opened
@@ -51,11 +52,16 @@
     [self.lifeLoop invalidate];
 }
 
+// only called once, when view is initiated
+- (void) viewInitiateSetup {
+    _appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    [_appDelegate initGame];
+    
+    _pirat = _appDelegate.pirate;
+}
+
 - (void) viewLoadSetup {
-    
-    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-    [appDelegate initGame];
-    
+
     self.desireLoop = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(updateDesires) userInfo:nil repeats:YES];
     self.lifeLoop = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(updateLifes)
                                                    userInfo:nil repeats:YES];
@@ -63,7 +69,6 @@
     // initialice storage and pirat
     _storage = [[Storage alloc] init];
     [_storage loadData];
-    _pirat = appDelegate.pirate;		
     
     // set up new game Thread
     [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(updateDesires) userInfo:nil repeats:YES];
@@ -119,7 +124,7 @@
     
     // gold Label and Heart Image
     _goldLabel.text = _storage.supplies[MONEY][AMOUNT];
-    
+
     int piratLife = _pirat.lifes;
     NSString *lifeImageString;
     switch (piratLife) {
