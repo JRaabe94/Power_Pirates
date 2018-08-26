471e34ef4ee41e1bd00756c9c8a103997f53eec2
Dominik <Codecamp@p200300d573d3ae227dbb81fce6a2ec73.dip0.t-ipconnect.de>
2018-08-26 13:04:54 -0700

    Kommentare

--- Power_Pirates/viewcontroller/ExploreMinigameViewController.m
+++ Power_Pirates/viewcontroller/ExploreMinigameViewController.m
@@ -31,7 +31,7 @@ CLLocation *lastLocation;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    locationManager = [[CLLocationManager alloc] init];
+    //locationManager = [[CLLocationManager alloc] init];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,30 +40,37 @@ CLLocation *lastLocation;
 }
 
 - (IBAction)onBackButton:(id)sender {
+    //stop location updates
     [locationManager stopUpdatingLocation];
-    lastLocation = nil;
     
     [self dismissViewControllerAnimated:YES completion:nil];
     
 }
 
 - (IBAction)onStartButton:(id)sender {
-    self.totalDistance = 0;
-    lastLocation = nil;
+    self.totalDistance = 0;     //init counter for traveled meters
+    lastLocation = nil;     //clear last location
+    
+    locationManager = [[CLLocationManager alloc] init];
     locationManager.delegate = self;
     locationManager.desiredAccuracy = kCLLocationAccuracyBest;
     
+    //label for feedback
     NSString *textForLabel = [NSString stringWithFormat:@"%f", 1000*distanceNeeded];
     _distanceLabel.text = [NSString stringWithFormat:@"%@%@", @"Verbleibende Meter: ", textForLabel];
 
     NSLog(@"Gestartet");
-        
+    
+    //Request Permission
     if ([locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
         [locationManager requestWhenInUseAuthorization];
     }
+    
+    //Start Location Updates
     [locationManager startUpdatingLocation];
 }
 
+//check if the player traveled the needed distance
 -(BOOL)checkWin{
     if(self.totalDistance > distanceNeeded){
         return true;
@@ -77,28 +84,31 @@ CLLocation *lastLocation;
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
 {
     NSLog(@"didFailWithError: %@", error);
-    //UIAlertController *errorAlert = [[UIAlertController initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-    //[errorAlert show];
 }
 
 -(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
     NSLog(@"%f", _totalDistance);
-    CLLocation *currentLocation = [locations lastObject];
+    CLLocation *currentLocation = [locations lastObject];   //get the new location values
     if(currentLocation != nil){
         if(lastLocation != nil){
-            CLLocationDistance meters = [lastLocation distanceFromLocation:currentLocation];
-            _totalDistance = _totalDistance + (meters/1000);
-            NSString *textForLabel = [NSString stringWithFormat:@"%f", ((distanceNeeded - _totalDistance) * 1000)];
+            CLLocationDistance distance = [lastLocation distanceFromLocation:currentLocation] / 1000;    //calculate the distance between the last and the current location in meters
+            _totalDistance = _totalDistance + (distance);      //add the newly traveled meters
+            
+            NSString *textForLabel = [NSString stringWithFormat:@"%f", ((distanceNeeded - _totalDistance) * 1000)];     //prepare string for label
             _distanceLabel.text = [NSString stringWithFormat:@"%@%@", @"Verbleibende Meter: ", textForLabel];
+            
+            //set the new last location for next update
             lastLocation = currentLocation;
         }else{
+            //set the lastLocation, if no last Location is set
             lastLocation = currentLocation;
         }
     }
     if([self checkWin]){
+        //if the player win, the updates should stop
         [locationManager stopUpdatingLocation];
-        _totalDistance = 0;
         
+        //give the player money
         Storage *storage = [[Storage alloc] init];
         [storage loadData];
         for(int i = 0; i < 10; i++){
